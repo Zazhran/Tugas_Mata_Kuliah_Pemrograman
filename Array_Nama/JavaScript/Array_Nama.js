4016fa64e69fb228d19419c8cc8c982be7329507
Zazhran <zazhran03@gmail.com>
2025-10-25 13:18:26 +0700

    Update Array_Nama.js

--- Array_Nama/JavaScript/Array_Nama.js
+++ Array_Nama/JavaScript/Array_Nama.js
@@ -1,1 +1,15 @@
+const namaMahasiswa = ["Ahmad", "John", "Akira", "Beethoven", "Xi Jinping"];
+alert("Daftar Mahasiswa:\n" + namaMahasiswa.join(", "));
+let namaInput = prompt("Masukkan Nama Mahasiswa: ").toLowerCase();
+let ditemukan = false;
 
+for (let i = 0; i < namaMahasiswa.length; i++){
+    if(namaMahasiswa[i].toLowerCase() === namaInput){
+        alert(`${namaMahasiswa[i]} ada di dalam list`);
+        ditemukan = true;
+        break;
+    }
+}
+if (!ditemukan){
+    alert("Nama tidak ada di dalam list");
+}
