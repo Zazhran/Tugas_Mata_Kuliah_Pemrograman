9d3cd0ca8731704969e02a1c51fdedcd10c644a9
Zazhran <zazhran03@gmail.com>
2025-10-25 13:02:15 +0700

    Update Array_Nama_Python.py

--- Array_Nama/Python/Array_Nama_Python.py
+++ Array_Nama/Python/Array_Nama_Python.py
@@ -2,20 +2,15 @@ nama_mahasiswa = ["Ahmad", "John", "Akira", "Beethoven", "Xi Jinping"]
 
 print(nama_mahasiswa)
 
-nama_input = input ("Masukkan Nama Mahasiswa\n")
+nama_input = input ("Masukkan Nama Mahasiswa\n").lower()
 
-if nama_input == ("Ahmad"):
-    print(nama_mahasiswa[0], " Ada di list")
+found = None
+for nama in nama_mahasiswa:
+    if nama.lower() == nama_input: #.lower() agar tidak mempedulikan kapital atau tidaknya
+        found = nama
+        break
     
-elif nama_input == ("John"):
-    print(nama_mahasiswa[1], "Ada di list")
-    
-elif nama_input == ("Akira"):
-    print(nama_mahasiswa[2], "Ada di list")
-elif nama_input == ("Beethoven"):
-    print(nama_mahasiswa[3], "Ada di list")
-elif nama_input == ("Xi Jinping"):
-    print(nama_mahasiswa[4], "Ada di list")
-    
+if found:
+    print(found, "Ada di list")
 else:
     print("Nama tidak ada di dalam list")
