f9aa3559d9f9f779536397326527f29fe6196039
Zazhran <zazhran03@gmail.com>
2025-10-25 11:18:15 +0700

    Update Leap_Year_JavaScript.js

--- Leap Year/JavaScript/Leap_Year_JavaScript.js
+++ Leap Year/JavaScript/Leap_Year_JavaScript.js
@@ -1,1 +1,21 @@
+let tahun = parseInt(prompt("Masukkan Tahun : "));
+let bulan = parseInt(prompt("Masukkan bulan : "));
+let hari;
 
+if (bulan === 2 ) 
+{
+    if((tahun % 400 === 0) || (tahun % 4 === 0 && tahun % 100 != 0))
+    {
+        hari = 29;
+    } else
+    {
+        hari = 28;
+    }
+} else if (([1, 3, 5, 7, 8, 10, 12]).includes(bulan))
+    {
+        hari = 31;
+    }else
+        {
+            hari = 30;
+        }
+console.log("Jumlah Hari :", hari);
